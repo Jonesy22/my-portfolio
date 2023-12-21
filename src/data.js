bf6296b4815c526a659f9483c007856514a6f00c
Daniel <danielmcjones@gmail.com>
2023-12-21 13:34:10 -0800

    adding more projects

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const projects = [
   {
     title: "Moodify",
-    subtitle: "Java Android App",
+    subtitle: "Android Mobile App",
     description:
       "An Android mobile app that allows users to select a mood, which then provides a list of songs, artists, and albums that correspond to their mood. The Spotify API was used.",
     technology: ["Java, Android Studio"],
@@ -9,53 +9,13 @@ export const projects = [
     link: "https://github.com/Jonesy22/Spotify-Music-Recommendation-App",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: "Trucker Tycoon",
+    subtitle: "React WebApp",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "DevChat",
-    subtitle: "React and Firebase",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
-  },
-];
-
-export const testimonials = [
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle",
-  },
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
-    company: "Dooble.io",
-  },
-];
-
-export const skills = [
-  "JavaScript",
-  "React",
-  "Next.js",
-  "Node",
-  "GraphQL",
-  "Material UI",
+      "An 'Oregon Trail' type  game where the player has to try and survive as long as they can without going bankrupt.\n" + 
+      "A somewhat realistic representation of the challenges carriers experience during their life on the road.",
+    technology: ["React, Typescript"],
+    image: "./Trucker-Tycoon.png",
+    link: "https://github.com/Jonesy22/trucker-tycoon",
+  }
 ];
