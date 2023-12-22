0a1906101d4978eb7494b15eabd4aed750b059f9
Daniel <danielmcjones@gmail.com>
2023-12-21 16:52:36 -0800

    adding more projects

--- src/components/About.js
+++ src/components/About.js
@@ -3,13 +3,12 @@ import React from "react";
 export default function About() {
 	return (
 	  <section id="about">
-		<div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-		  <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-			<h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-			&nbsp;Hi, I'm Daniel.
-			  <br className="hidden lg:inline-block" />&nbsp;I enjoy building cool systems.
+		<div className="container mx-auto flex px-96 py-80 md:flex-row flex-col items-center">
+		  <div className="lg:flex-grow md:w-1/2 flex flex-col md:items-start mb-16 md:mb-0 items-center text-center">
+			<h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white mx-auto my-auto">
+			I'm Daniel, a Full Stack Developer living in Portland, OR.
 			</h1>
-			<div className="flex justify-center">
+			<div className="flex justify-center align-center mx-auto my-auto">
 			  <a
 				href="#contact"
 				className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
@@ -21,13 +20,6 @@ export default function About() {
 				See My Past Work
 			  </a>
 			</div>
-		  </div>
-		  <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-			<img
-			  className="object-cover object-center rounded"
-			  alt="daniel"
-			  src="./selfie.png"
-			/>
 		  </div>
 		</div>
 	  </section>
--- src/data.js
+++ src/data.js
@@ -2,20 +2,34 @@ export const projects = [
   {
     title: "Moodify",
     subtitle: "Android Mobile App",
-    description:
-      "An Android mobile app that allows users to select a mood, which then provides a list of songs, artists, and albums that correspond to their mood. The Spotify API was used.",
+    description: "An Android mobile app that allows users to select a mood, which then provides a list of songs, artists, and albums.",
     technology: ["Java, Android Studio"],
-    image: "./MoodifySnap.PNG",
+    image: "./MoodifySnap.png",
     link: "https://github.com/Jonesy22/Spotify-Music-Recommendation-App",
   },
   {
     title: "Trucker Tycoon",
     subtitle: "React WebApp",
-    description:
-      "An 'Oregon Trail' type  game where the player has to try and survive as long as they can without going bankrupt.\n" + 
-      "A somewhat realistic representation of the challenges carriers experience during their life on the road.",
+    description: "An 'Oregon Trail' type  game where the player has to try and survive as long as they can without going bankrupt.",
     technology: ["React, Typescript"],
     image: "./Trucker-Tycoon.png",
     link: "https://github.com/Jonesy22/trucker-tycoon",
-  }
+  },
+  {
+    title: "Perro",
+    subtitle: "React Webapp",
+    description: "A lightweight project management velocity tracker used by small development teams to quickly and accurately track their work.",
+    technology: ["React, Javascript"],
+    image: "./Perro.png",
+    link: "https://github.com/Jonesy22/Perro",
+  },
+  {
+    title: "Othello",
+    subtitle: "Python (terminal game)",
+    description: "A terminal based board game called Othello, where you (the user) play against an unbeatable opponent.",
+    technology: ["Python, AI"],
+    image: "./othello.png",
+    link: "https://github.com/Jonesy22/Othello",
+  },
+
 ];
