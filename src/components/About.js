fd8489b9f67dff458c434b7b7363ea982649d5d5
Daniel <danielmcjones@gmail.com>
2023-12-21 21:37:15 -0800

    fix: fixing dimensions for mobile

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
 	return (
 	  <section id="about">
-		<div className="container mx-auto flex px-96 py-80 md:flex-row flex-col items-center">
+		<div className="container mx-auto flex pt-80 pb-80 md:flex-row flex-col items-center">
 		  <div className="lg:flex-grow md:w-1/2 flex flex-col md:items-start mb-16 md:mb-0 items-center text-center">
 			<h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white mx-auto my-auto">
 			I'm Daniel, a Full Stack Developer living in Portland, OR.
