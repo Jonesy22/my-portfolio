f7d8c9e9c3ccf430c216b3033ee564df346566ee
Daniel <danielmcjones@gmail.com>
2024-04-12 11:18:40 -0700

    setting up itinerary

---
+++ src/components/Itinerary.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+export default function Itinerary() {
+    return (
+        <div class="text-center">
+            <div>
+                <h1 class="font-bold text-5xl mt-10" >Friday Schedule</h1>
+                    <ul class="m-10">
+                        <li class="m-4"><b>7:00pm-10:00pm:</b> Pack up everything needed for the Saturday - decide where to go for breakfast tommorrow</li>
+                    </ul>
+            </div>
+           <div>
+           <h1 class="font-bold text-5xl mt-10">Saturday Schedule</h1>
+            <ul class="m-10">
+                <li class="m-4"><b>6:30am:</b> Wakeup and get ready to attack the day</li>
+                <li class="m-4"><b>7:00am:</b> Head out to breakfast with all our stuff</li>
+                <li class="m-4"><b>8:00am:</b> Embark on journey to Saddle Mountain</li>
+                <li class="m-4"><b>9:30am:</b> Arrive at SM, engage in dynamic stretches</li>
+                <li class="m-4"><b>9:45am:</b> Start walking on dirt path to find the saddle!</li>
+                <li class="m-4"><b>1:00pm:</b> Arrive back at car, having found the saddle</li>
+                <li class="m-4"><b>1:15pm:</b> Head back to DG</li>
+                <li class="m-4"><b>2:30pm</b>: Grab a light quick lunch somewhere</li>
+                <li class="m-4"><b>2:45pm:</b> Clean up, welcome Deleware Diaz with open arms</li>
+                <li class="m-4"><b>3:00pm:</b> Hangout at DG, relax and ice muscles if needed</li>
+                <li class="m-4"><b>6:00pm:</b> Head to dinner reservation</li>
+                <li class="m-4"><b>6:30pm:</b> Eat dinner, have laughs, enjoy company</li>
+                <li class="m-4"><b>7:45pm:</b> Ellie's choice - unplanned time to head back to DG, go to bar, do whatever bday girl wants</li>
+            </ul>
+           </div>
+           <div>
+           <h1 class="font-bold text-5xl mt-10">Sunday Schedule</h1>
+            <ul class="m-10">
+                <li class="m-4"><b>8:00am:</b> Wakeup and get ready to attack the day, again!</li>
+                <li class="m-4"><b>8:15am:</b> Head out to breakfast</li>
+                <li class="m-4"><b>9:15am:</b> Head to Woodburn to visit Wooden Shoe Tulip Festival</li>
+                <li class="m-4"><b>10:00am:</b> Arrive at Tulip Fest, start looking at tulips</li>
+                <li class="m-4"><b>11:45am:</b> Finish tulip watching</li>
+                <li class="m-4"><b>12:00pm:</b> Head back to DG area</li>
+                <li class="m-4"><b>12:45pm:</b> Go to establishment to consume food</li>
+                <li class="m-4"><b>1:45pm-2:00pm</b>: Say farewell to DD as she goes back North</li>
+                <li class="m-4"><b>2:00pm+:</b> Ellie's choice, hang out, relax, go outside</li>
+            </ul>
+           </div>
+           <div>
+           <h1 class="font-bold text-xs mt-10 mt-22">Monday Schedule</h1>
+            <ul class="m-2 text-xs">
+                <li class="m-4"><b>12:00am:</b> No longer Ellie's day of birth</li>
+            </ul>
+           </div>
+        </div>
+
+      );
+    }
---
+++ src/tasks.js
@@ -0,0 +1,7 @@
+export const tasks = [
+    { time: '7am', task: 'Do yoga and meditation.' },
+    { time: '8am', task: 'Breakfast and read news.' },
+    { time: '9am', task: 'Start working on the project.' },
+    { time: '10am', task: 'Attend team meeting via Zoom.' },
+    { time: '11am', task: 'Review emails and respond.' }
+]
